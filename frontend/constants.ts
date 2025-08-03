ae1cb2013ae3077dea096ee2bf77ec6a70459c48
mrdragar <yshhenyaev@mail.ru>
2025-08-03 19:05:00 +0300

    Comment constans line

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -5,6 +5,6 @@ export const NAVIGATION_ITEMS: NavItem[] = [
   { name: 'Программа ЛДПР по защите людей', path: '/program' },
   { name: 'Подать обращение', path: '/contact' },
   { name: 'Защита животных', path: '/animal-protection' },
-  { name: 'Единый день голосования', path: '/voting-day' },
+//  { name: 'Единый день голосования', path: '/voting-day' },
   { name: 'Помощь молодой семье', path: '/social-sphere' },
 ];
